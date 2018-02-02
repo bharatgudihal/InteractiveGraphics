e9f6a1d067f8f1e0ee1dd3a27aceb86095b18405
Bharat Gudihal <bharatgudihal@users.noreply.github.com>
2018-02-01 22:29:00 -0700

    changed back to camera space

--- InteractiveGraphics/Assets/Shaders/Vertex/mesh.glsl
+++ InteractiveGraphics/Assets/Shaders/Vertex/mesh.glsl
@@ -4,7 +4,7 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 
 uniform mat4 mvp;
-uniform mat3 worldMatrix;
+uniform mat4 cameraMatrix;
 uniform mat3 normalMatrix;
 
 out vec3 o_normal;
@@ -13,5 +13,5 @@ out vec3 o_position;
 void main(){
 	gl_Position = mvp * vec4(position, 1.0);
 	o_normal = normalMatrix * normal;
-	o_position = worldMatrix * position;
+	o_position = vec3(cameraMatrix * vec4(position, 1.0));
 }
